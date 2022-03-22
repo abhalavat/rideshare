a78480fb0c2b4d01975eb78a07b778e8982766dc
abhalavat <abhalavat@utexas.edu>
2022-03-21 23:11:52 -0500

    update login

--- screens/Login.js
+++ screens/Login.js
@@ -1,22 +1,30 @@
-import { Text, TouchableOpacity, View, TextInput } from 'react-native';
+import { ImageBackground, Text, TouchableOpacity, View, TextInput, Image } from 'react-native';
 import { useEffect, useState } from 'react';
 import styles from '../styles/LoginStyles';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import icon from '../assets/icon.png';
+import background from '../assets/start-background.png';
+import text from '../assets/text-logo-white.png';
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [invalidEmail, setInvalidEmail] = useState(true);
   const navigation = props.navigation;
 
   const user = null;
   const auth = getAuth();
 
   const handleSignUp = () => {
-    createUserWithEmailAndPassword(auth, email, password)
-        .then(userCredentials => {
-            const user = userCredentials.user;
-        })
-        .catch(error => alert(error.message));
+    if (email.substring(email.length - 11) =='@utexas.edu') {
+      createUserWithEmailAndPassword(auth, email, password)
+          .then(userCredentials => {
+              const user = userCredentials.user;
+          })
+          .catch(error => alert(error.message));
+    } else {
+      alert('Email must be a valid utexas.edu email!');
+    }
   }
 
   const handleSignIn = () => {
@@ -25,7 +33,15 @@ export default function Login(props) {
         const user = userCredentials.user;
         navigation.navigate("Feed");
       })
-      .catch(error => alert(error.message));
+      .catch(error => {
+        if (error.message == "Firebase: Error (auth/user-not-found)."){
+          alert("An account does not exist with that email. Please create an account.");
+        } else if (error.message == "Firebase: Error (auth/wrong-password)."){
+          alert("Incorrect Password. Please try again.");
+        } else {
+          alert(error.message);
+        }
+      });
   }
 
   useEffect(() => {
@@ -39,25 +55,33 @@ export default function Login(props) {
 
   return (
     <View style={styles.screen}>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={text => setEmail(text)}
-        style={styles.inputText}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={text => setPassword(text)}
-        style={styles.inputText}
-        secureTextEntry
-      />
-      <TouchableOpacity style={styles.button} onPress={handleSignIn}>
-          <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
-          <Text style={styles.buttonText}>Register</Text>
-      </TouchableOpacity>
+      <ImageBackground style={styles.background} source={background}>
+        <View style={styles.logoContainer}>
+          <Image source={icon} style={styles.img}/>
+        </View>
+        <View style={styles.textContainer}>
+          <Image source={text} style={styles.img}/>
+        </View>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.inputText}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.inputText}
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+            <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+            <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
--- styles/LoginStyles.js
+++ styles/LoginStyles.js
@@ -32,5 +32,39 @@ export default StyleSheet.create({
     },
     buttonText: {
         fontSize: 25,
-    }
+    },
+    errorText: {
+        marginTop: '2%',
+        fontSize: 20,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10,
+        color: 'red',
+        padding: '1%',
+    },
+    img: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain',
+    },
+    logoContainer: {
+        width: '25%',
+        height: '10%',
+        marginTop: '25%',
+        // borderWidth: 1,
+        // borderColor: 'black',
+    },
+    textContainer: {
+        width: '80%',
+        height: '10%',
+        marginBottom: '20%',
+        // borderWidth: 1,
+        // borderColor: 'black',
+    },
+    background: {
+        resizeMode: 'contain',
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+    },
 })
